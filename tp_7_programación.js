824b83fed0dfc20d79431e19a73e186830289f9d
luzlomba <lombardimarialuz@gmail.com>
2023-11-28 20:55:15 -0300

    se suben lista de cuentas

---
+++ tp_7_programación.js
@@ -0,0 +1,115 @@
+const cuenta1 = {
+    tipo: "corriente", // "ahorro"
+    importe: 200.000,
+    tipoMoneda: "peso", // "euro", // "dolar",
+    plazoFijo: [{
+    estado: "activo", // "inactivo"
+    importe: 100.000,
+    fechaCreacion: "2020-01-01",
+    plazo: 30, // en dias
+    }],
+    }
+
+const cuenta2 = {
+    tipo: "ahorro",
+    importe: 300.000,
+    tipoMoneda: "dolar", // "euro", // "dolar",
+    plazoFijo: [{
+    estado: "inactivo", // "inactivo"
+    importe: 200.000,
+    fechaCreacion: "2020-06-10",
+    plazo: 60, // en dias
+    }],
+    }
+const cuenta3 = {
+    tipo: "ahorro", // "ahorro", "corriente"
+    importe: 50.000,
+    tipoMoneda: "dolar", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "inactivo", // "inactivo", // "activo"
+    importe: 50.000,
+    fechaCreacion: "2021-10-25",
+    plazo: 45, // en dias
+    }],
+    }
+const cuenta4 = {
+    tipo: "corriente", // "ahorro", "corriente"
+    importe: 150.000,
+    tipoMoneda: "euro", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "activo", // "inactivo", // "activo"
+    importe: 100.000,
+    fechaCreacion: "2022-12-18",
+    plazo: 45, // en dias
+    }],
+    }
+const cuenta5 = {
+    tipo: "ahorro", // "ahorro", "corriente"
+    importe: 50.000,
+    tipoMoneda: "peso", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "activo", // "inactivo", // "activo"
+    importe: 10.000,
+    fechaCreacion: "2023-01-25",
+    plazo: 90, // en dias
+    }],
+    }
+
+const cuenta6 = {
+    tipo: "corriente", // "ahorro", "corriente"
+    importe: 20.000,
+    tipoMoneda: "peso", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "activo", // "inactivo", // "activo"
+    importe: 20.000,
+    fechaCreacion: "2023-11-28",
+    plazo: 120, // en dias
+    }],
+    }
+const cuenta7 = {
+    tipo: "ahorro", // "ahorro", "corriente"
+    importe: 250.000,
+    tipoMoneda: "dolar", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "activo", // "inactivo", // "activo"
+    importe: 50.000,
+    fechaCreacion: "2021-08-14",
+    plazo: 60, // en dias
+    }],
+    }
+const cuenta8 = {
+    tipo: "ahorro", // "ahorro", "corriente"
+    importe: 130.000,
+    tipoMoneda: "peso", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "inactivo", // "inactivo", // "activo"
+    importe: 30.000,
+    fechaCreacion: "2022-05-22",
+    plazo: 120, // en dias
+    }],
+    }
+const cuenta9 = {
+    tipo: "ahorro", // "ahorro", "corriente"
+    importe: 180.000,
+    tipoMoneda: "peso", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "activo", // "inactivo", // "activo"
+    importe: 50.000,
+    fechaCreacion: "2023-07-03",
+    plazo: 30, // en dias
+    }],
+    }
+const cuenta10 = {
+    tipo: "corriente", // "ahorro", "corriente"
+    importe: 100.000,
+    tipoMoneda: "euro", // "euro", // "dolar", // "peso"
+    plazoFijo: [{
+    estado: "activo", // "inactivo", // "activo"
+    importe: 50.000,
+    fechaCreacion: "2022-03-16",
+    plazo: 90, // en dias
+    }],
+    }
+
+const cuentas = [cuenta1, cuenta2, cuenta3, cuenta4, cuenta5, cuenta6, cuenta7, cuenta8, cuenta9, cuenta10]
+
