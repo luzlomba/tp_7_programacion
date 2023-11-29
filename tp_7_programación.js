78043390dfb33aa0a707e25d1b5e4ecfac21d150
luzlomba <lombardimarialuz@gmail.com>
2023-11-28 23:20:34 -0300

    segundo commit

--- tp_7_programación.js
+++ tp_7_programación.js
@@ -1,10 +1,10 @@
 const cuenta1 = {
     tipo: "corriente", // "ahorro"
-    importe: 200.000,
+    importe: 200000,
     tipoMoneda: "peso", // "euro", // "dolar",
     plazoFijo: [{
     estado: "activo", // "inactivo"
-    importe: 100.000,
+    importe: 100000,
     fechaCreacion: "2020-01-01",
     plazo: 30, // en dias
     }],
@@ -12,44 +12,44 @@ const cuenta1 = {
 
 const cuenta2 = {
     tipo: "ahorro",
-    importe: 300.000,
+    importe: 300000,
     tipoMoneda: "dolar", // "euro", // "dolar",
     plazoFijo: [{
     estado: "inactivo", // "inactivo"
-    importe: 200.000,
+    importe: 200000,
     fechaCreacion: "2020-06-10",
     plazo: 60, // en dias
     }],
     }
 const cuenta3 = {
     tipo: "ahorro", // "ahorro", "corriente"
-    importe: 50.000,
+    importe: 50000,
     tipoMoneda: "dolar", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "inactivo", // "inactivo", // "activo"
-    importe: 50.000,
+    importe: 50000,
     fechaCreacion: "2021-10-25",
     plazo: 45, // en dias
     }],
     }
 const cuenta4 = {
     tipo: "corriente", // "ahorro", "corriente"
-    importe: 150.000,
+    importe: 150000,
     tipoMoneda: "euro", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "inactivo", // "inactivo", // "activo"
-    importe: 100.000,
+    importe: 100000,
     fechaCreacion: "2022-12-18",
     plazo: 45, // en dias
     }],
     }
 const cuenta5 = {
     tipo: "ahorro", // "ahorro", "corriente"
-    importe: 50.000,
+    importe: 50000,
     tipoMoneda: "peso", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "activo", // "inactivo", // "activo"
-    importe: 10.000,
+    importe: 10000,
     fechaCreacion: "2023-01-25",
     plazo: 90, // en dias
     }],
@@ -57,55 +57,55 @@ const cuenta5 = {
 
 const cuenta6 = {
     tipo: "corriente", // "ahorro", "corriente"
-    importe: 20.000,
+    importe: 20000,
     tipoMoneda: "peso", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "activo", // "inactivo", // "activo"
-    importe: 20.000,
+    importe: 20000,
     fechaCreacion: "2023-11-28",
     plazo: 120, // en dias
     }],
     }
 const cuenta7 = {
     tipo: "ahorro", // "ahorro", "corriente"
-    importe: 250.000,
+    importe: 250000,
     tipoMoneda: "dolar", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "activo", // "inactivo", // "activo"
-    importe: 50.000,
+    importe: 50000,
     fechaCreacion: "2021-08-14",
     plazo: 60, // en dias
     }],
     }
 const cuenta8 = {
     tipo: "ahorro", // "ahorro", "corriente"
-    importe: 130.000,
+    importe: 130000,
     tipoMoneda: "peso", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "inactivo", // "inactivo", // "activo"
-    importe: 30.000,
+    importe: 30000,
     fechaCreacion: "2022-05-22",
     plazo: 120, // en dias
     }],
     }
 const cuenta9 = {
     tipo: "ahorro", // "ahorro", "corriente"
-    importe: 180.000,
+    importe: 180000,
     tipoMoneda: "peso", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "activo", // "inactivo", // "activo"
-    importe: 50.000,
+    importe: 50000,
     fechaCreacion: "2023-07-03",
     plazo: 30, // en dias
     }],
     }
 const cuenta10 = {
     tipo: "corriente", // "ahorro", "corriente"
-    importe: 100.000,
+    importe: 1000000,
     tipoMoneda: "euro", // "euro", // "dolar", // "peso"
     plazoFijo: [{
     estado: "activo", // "inactivo", // "activo"
-    importe: 50.000,
+    importe: 600000,
     fechaCreacion: "2022-03-16",
     plazo: 90, // en dias
     }],
@@ -175,3 +175,31 @@ console.log (pfPorMoneda (cuentas , "euro"))
 // e) Realiza una función que permita descontar un importe dado (mantenimiento de cuenta) a
 // todas las cuentas en la lista.
 
+cuentasModificadas = []
+function mantenimientoDeCuenta ( listadoCuentas , descuentoPorMantenimiento){
+    for (let index = 0; index < listadoCuentas.length; index++) {
+        nuevoSaldo = listadoCuentas[index].importe - descuentoPorMantenimiento
+        listadoCuentas[index].importe = nuevoSaldo
+        cuentasModificadas.push (listadoCuentas[index])
+    }
+    return cuentasModificadas
+}
+console.log (mantenimientoDeCuenta (cuentas , 25))
+
+// f) Realiza una función que permita incrementar un porcentaje dado (por veneficios
+// exclusivos) a todas las cuentas con plazo fijo con importe mayor a 500000.
+
+cuentasConBeneficio = []
+function beneficioExclusivo (listadoCuentas , incremento) {
+    for (let index = 0; index < listadoCuentas.length; index++) {
+        if (listadoCuentas[index].plazoFijo[0].importe > 500000){
+        saldoConBeneficio = listadoCuentas[index].importe + (listadoCuentas[index].importe * incremento / 100)
+        listadoCuentas[index].importe = saldoConBeneficio
+        cuentasConBeneficio.push (listadoCuentas[index])
+        }
+    }
+    return cuentasConBeneficio
+}
+console.log (beneficioExclusivo (cuentas , 10))
+
+// g) Realiza una función que permita cargar una nueva cuenta al final de la lista.
